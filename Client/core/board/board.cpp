f20c22aa9c6e6ca6f0d98025a85116b5601de103
Gaetano Di Tota <ditotagaetano@outlook.it>
2026-02-11 17:05:46 +0100

    Added board class implementation

---
+++ Client/core/board/board.cpp
@@ -0,0 +1,56 @@
+#include "board.hpp"
+
+namespace lso {
+
+    void Board::printBoard() {
+        for (unsigned int row = 0; row < rows; row++)
+            Board::printRow(row);
+
+            Board::printSeparator();
+            Board::printColumnNumbers();
+    }
+
+
+    void Board::printRow(const unsigned int row) {
+        std::cout << TextColor::BLUE << '|' << TextColor::WHITE;
+
+        for (unsigned int column = 0; column < columns; column++)
+            std::cout << " " << grid[row][column] << " ";
+
+        std::cout << TextColor::BLUE << '|' << TextColor::WHITE << std::endl;
+    }
+
+    void Board::printSeparator() {
+        std::cout << TextColor::BLUE << '+';
+
+        for (unsigned int column = 0; column < columns; column++)
+            std::cout << "---";
+
+        std::cout << '+' << TextColor::WHITE << std::endl;
+    }
+
+    void Board::printColumnNumbers() {
+        std::cout << TextColor::BLUE << '|' << TextColor::WHITE;
+
+        for (unsigned int i = 0; i < columns; i++) {
+            std::cout << ' ' << i << ' ';
+        }
+
+        std::cout << TextColor::BLUE << '|' << TextColor::WHITE << std::endl;
+    }
+
+    void Board::addMove(const unsigned int column, const TextColor color) {        
+        unsigned int row = findDeepestEmptyRow(column);
+
+        grid[row][column] = color + playerSymbol + TextStyle::RESET;
+    }
+
+    unsigned int Board::findDeepestEmptyRow(const unsigned int column) {
+        for (unsigned int row = rows - 1; row != invalidGridValue; row--)
+            if (grid[row][column] == emptySymbol)
+                return row;
+
+        throw std::logic_error("Expected to find an empty row...");
+    }
+
+}
