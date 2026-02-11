7bc4ee2529f17c4dda6d432ab41f68bb1882988f
Gaetano Di Tota <ditotagaetano@outlook.it>
2026-02-11 18:13:26 +0100

    Refactor of UDPClient class

---
+++ Client/core/network/UDPClient.cpp
@@ -0,0 +1,53 @@
+#include "UDPClient.hpp"
+
+namespace lso {
+
+    UDPClient::UDPClient(std::string server_ip, int server_port) {
+        clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
+
+        if (clientSocket < 0)
+            throw std::runtime_error("Cannot create socket...");
+
+        memset(&serverAddress, 0, sizeof(serverAddress));
+
+        serverAddress.sin_family = AF_INET;
+        serverAddress.sin_port = htons(server_port);
+        serverAddress.sin_addr.s_addr = inet_addr(server_ip.c_str());
+    }
+
+    UDPClient::~UDPClient() { close(clientSocket); }
+
+    void UDPClient::sendMessage(std::string message) {
+        sendto(
+            clientSocket,
+            message.c_str(),
+            message.length(),
+            MSG_WAITALL,
+            (const struct sockaddr *) &serverAddress,
+            sizeof(serverAddress)
+        );
+    }
+
+    std::string UDPClient::receiveMessage() {
+        char buffer[MAXLINE];
+
+        socklen_t socketLength = sizeof(serverAddress);
+
+        ssize_t n = recvfrom(
+            clientSocket,
+            buffer,
+            MAXLINE,
+            MSG_WAITALL,
+            (struct sockaddr *) &serverAddress,
+            &socketLength
+        );
+        
+        if (n < 0)
+            return std::string("");
+
+        buffer[n] = '\0';
+
+        return std::string(buffer);
+    }
+
+}
---
+++ Client/core/network/UDPClient.hpp
@@ -0,0 +1,38 @@
+#ifndef UDPCLIENT_HPP
+#define UDPCLIENT_HPP
+
+#include <string>
+#include <unistd.h>
+#include <stdexcept>    
+#include <sys/socket.h> 
+#include <arpa/inet.h>
+
+namespace lso {
+
+    class UDPClient {
+        private:
+
+            int clientSocket;
+
+            struct sockaddr_in serverAddress;
+            
+            const int MAXLINE = 1024;
+
+        protected:
+
+            // ...
+
+        public:
+
+            UDPClient(std::string server_ip, int server_port);
+            
+            ~UDPClient();
+
+            void sendMessage(std::string message);
+
+            std::string receiveMessage();
+    };
+
+}
+
+#endif
