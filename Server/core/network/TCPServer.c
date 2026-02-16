6b6f68cab5e7e65ac297aa16bb75ea44d132aa12
eman-giaquinto <eman.giaquinto@studenti.unina.it>
2026-02-16 21:16:29 +0100

    Refactoring del file dedicato alla sola connessione e avvio del Server

--- Server/core/network/TCPServer.c
+++ Server/core/network/TCPServer.c
@@ -1,5 +1,7 @@
 #include "TCPServer.h"
-#include "../shared/contracts.h"
+#include "../utils/list.h"
+#include "../threading/threads.h"
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,10 +17,6 @@
 #include <pthread.h>
 
 #define PORTA 5000
-#define DIMENSIONE_BUFFER 1024
-
-
-ListaClient* listaClient = NULL;
 
 int creaSocketServerTCP(){
         int socketServer;
@@ -78,98 +76,9 @@ void avviaServer(int socketServer){
 
                 stampaLista(); // only for debug da togliere peffozza
 
-                associaThreadClient(nuovoClient);
+                associaThreadClient(nuovoClient); 
                 
         } 
 }
 
-void associaThreadClient(int nuovoClientConnesso) {
-        // Creo il thread
-        pthread_t tid;
 
-        int *clientInfo = malloc(sizeof(int));
-        *clientInfo = nuovoClientConnesso;
-
-        if (pthread_create(&tid, NULL, gestioneClientThread,clientInfo) != 0) {
-                perror("Errore creazione thread");
-        }
-
-}
-
-void *gestioneClientThread(void *arg){
-        int socketClient = *(int*)arg;
-        free(arg); // Rilascio subito la memoria utilizzata e la rendo disponibile immediatamente
-
-        char bufferMessaggi[DIMENSIONE_BUFFER];
-        ssize_t byteLetti;
-
-        while(1) 
-        {
-                byteLetti = recv(socketClient, &bufferMessaggi, sizeof(bufferMessaggi), 0);
-
-                if (byteLetti == 0) {
-                        printf("Client disconnesso (Socket %d)\n", socketClient);
-                        break; // Usciamo dal loop e cancellazione di quell' elemento dalla lista
-                }
-
-                else if (byteLetti < 0) {
-                        perror("Errore recv");
-                        break;
-                }
-
-                // fare operazioni varie e introdurre logica di gestione del tipo di messaggi ricevuti
-
-                // SCENARIO 1: CLIENT APPENA CONNESSO - SCOPO: INVIO MESSAGGIO DI BENVENUTO
-                // S_Benvenuto pacchetto;
-                // pacchetto.tipoMessaggio=S_BENVENUTO;
-                // strncpy(pacchetto.messaggioBenvenuto,"Ciao <nomeClient> ti diamo il benvenuto a Forza 4!",sizeof(pacchetto.messaggioBenvenuto));
-                        
-                //     char risposta[DIMENSIONE_BUFFER];
-                //     strcpy(risposta, "Messaggio ricevuto no corrispondenza dal Server!");
-                //     sendto(dati->socketServer, risposta, strlen(risposta), 0, (struct sockaddr *)&dati->indirizzoClient, sizeof(dati->indirizzoClient));    
-
-        }
-
-        close(socketClient); // client disconnesso mi chiudo la socket dal server verso quel client
-
-        // aggiungere rimozione in lista di quello specifico client
-
-        pthread_exit(NULL);
-        
-}
-
-// Implementazione per le funzioni dedicate alle liste
-void aggiungiClient(int nuovoClientConnesso){
-        ListaClient *nuovoNodo,*tempNodo;
-        nuovoNodo=malloc(sizeof(ListaClient));
-
-        nuovoNodo->socketClient=nuovoClientConnesso;
-        nuovoNodo->next=NULL;
-
-        if(listaClient==NULL){
-                listaClient=nuovoNodo;
-        }
-        else
-        {
-                tempNodo=listaClient;
-                nuovoNodo->next=tempNodo;
-                listaClient=nuovoNodo;
-        }
-
-}
-
-// per debug (da togliere)
-void stampaLista(){
-        if(listaClient==NULL){
-              printf("\n La lista e' vuota\n");  
-        }
-        else
-        {
-                printf("\n Lista: ");
-                while(listaClient!=NULL)
-                {
-                        printf("\n %d\n",listaClient->socketClient);
-                        listaClient=listaClient->next;
-                }  
-        }
-}
--- Server/core/network/TCPServer.h
+++ Server/core/network/TCPServer.h
@@ -5,22 +5,8 @@
 #include <sys/socket.h> // per sock_addr_in
 #include <netinet/in.h> // per sock_addr_in
 
-// Strutture dati necessarie
-typedef struct ListaClient {
-        int socketClient;
-        struct ListaClient *next;
-} ListaClient;
-
-// Funzioni per liste
-// inserimento in testa - ricerca - cancellazione di un determinato nodo
-void aggiungiClient(int nuovoClientConnesso);
-void stampaLista();
-
-
-
 int creaSocketServerTCP();
 void avviaServer(int socketServer);
-void associaThreadClient(int nuovoClientConnesso);
-void *gestioneClientThread(void *arg);
+
 
 #endif
