f8337db89d3d6a68fa61151778fa940ad56f292b
eman-giaquinto <eman.giaquinto@studenti.unina.it>
2026-02-17 14:15:13 +0100

    Refactoring del file delle variabili condivise

--- Server/core/global/globals.c
+++ Server/core/global/globals.c
@@ -3,3 +3,23 @@
 // Inizializzazioni
 pthread_mutex_t mutexListaClient = PTHREAD_MUTEX_INITIALIZER; 
 ListaClient* listaClient = NULL;
+
+pthread_mutex_t mutexListaPartite = PTHREAD_MUTEX_INITIALIZER; 
+ListaPartite* listaPartite = NULL;
+
+        // Game p1;
+        // p1.id=1;
+        // strcpy(p1.proprietario,"Mario");
+        // strcpy(p1.avversario,"");
+        // p1.stato=1u;
+
+        // aggiungiPartitaAllaLista(p1);
+
+        // p1.id=2;
+        // strcpy(p1.proprietario,"Nicola");
+        // strcpy(p1.avversario,"Maria");
+        // p1.stato=2;
+
+        // aggiungiPartitaAllaLista(p1);
+
+        // stampaListaPartite();
--- Server/core/global/globals.h
+++ Server/core/global/globals.h
@@ -3,10 +3,24 @@
 
 #include "../utils/list.h"
 #include <pthread.h>
+#include "../shared/protocol.h"
+
+typedef struct ListaClient {
+        int socketClient;
+        struct ListaClient *next;
+} ListaClient;
+
+typedef struct ListaPartite {
+        Game game;
+        struct ListaPartite *next;
+} ListaPartite;
+
 
 // Dichiarazioni
 extern pthread_mutex_t mutexListaClient; 
 extern ListaClient* listaClient;
 
+extern pthread_mutex_t mutexListaPartite; 
+extern ListaPartite* listaPartite;
 
 #endif
