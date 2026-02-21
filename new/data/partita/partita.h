9bda0f2f5ba84ae520d0847e0a7d601d2498d1a3
eman-giaquinto <eman.giaquinto@studenti.unina.it>
2026-02-21 18:52:21 +0100

    Aggiunta la logica lato server per determinare l'esito della partita

--- new/data/partita/partita.h
+++ new/data/partita/partita.h
@@ -2,11 +2,18 @@
 #define PARTITA_H
 
 #include "../client/client.h"
+#include <pthread.h>
+#include "../../network/shared/protocol.h"
+
+#define SIMBOLO_PROPRIETARIO 1
+#define SIMBOLO_AVVERSARIO 2
 
 typedef struct Partita {
     unsigned int id;
     Client * proprietario;
     Client * avversario;
+    pthread_mutex_t mutex;
+    unsigned int board[BOARD_ROWS][BOARD_COLUMNS];
     Partita * next;
 } Partita;
 
@@ -14,6 +21,17 @@ Partita * creaPartita(unsigned int id, Client * proprietario);
 
 void aggiungiProprietario(Partita * partita, Client * proprietario);
 void aggiungiAvversario(Partita * partita, Client * avversario);
+
+void aggiungiMossa(Partita *partita, int colonna, unsigned int simbolo);
+void aggiungiMossaProprietario(Partita *partita, unsigned int colonna){aggiungiMossa(partita, colonna, SIMBOLO_PROPRIETARIO);}
+void aggiungiMossaAvversario(Partita *partita, unsigned int colonna){aggiungiMossa(partita, colonna, SIMBOLO_AVVERSARIO);}
+
+unsigned int esitoPartita(Partita *partita,unsigned int simbolo); // 2 diagonali, orzziontale e verticale (capire come avvisare chi ha vinto, nella stessa funzione?) 
+unsigned int verificaCombinazioneOrizzontale(Partita *partita,unsigned int simbolo); 
+unsigned int verificaCombinazioneVerticale(Partita *partita,unsigned int simbolo); 
+unsigned int verificaCombinazioneDiagonalePrincipale(Partita *partita,unsigned int simbolo);
+unsigned int verificaCombinazioneDiagonaleSecondaria(Partita *partita,unsigned int simbolo); 
+unsigned int verificaPareggio(Partita *partita); 
 
 void rimuoviProprietario(Partita * partita);
 
