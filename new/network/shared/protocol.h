df6c528c7c6492b5b57c9848ff1e09feb7907636
Gaetano Di Tota <ditotagaetano@outlook.it>
2026-02-23 18:44:39 +0100

    Aggiornamento del protocollo da Request-Response a Event-Driven

--- new/network/shared/protocol.h
+++ new/network/shared/protocol.h
@@ -30,44 +30,38 @@ typedef struct {
 // (EVT = Event) Eventi inviati dal server
 
 typedef enum {
-    REQ_CONNECT = 1,
-    RES_CONNECT_ACCEPTED,
+    REQ_CONNECT = 1, // Payload = char nome[NAME_LEN]
 
     REQ_DISCONNECT,
 
     REQ_CREATE_GAME,
-    EVT_GAME_CREATED,
-    EVT_OPPONENT_JOIN,
-
+    RES_CREATE_GAME, // Payload = Game
+    
     REQ_JOIN_GAME,
-    EVT_GAME_ACCEPTED,
-    EVT_GAME_DENIED,
-
+    EVT_OPPONENT_JOIN_GAME,
+    REQ_RESPOND_OPPONENT_JOIN_GAME, // Payload = Boolean
+    RES_JOIN_GAME, // Payload = Boolean
+    
     REQ_LEAVE_GAME,
-
-    REQ_GAME_ACCEPTED,
-
-    REQ_GAME_DENIED,
-
+    
     REQ_GAMES_LIST,
-    RES_GAMES_LIST,
-
-    EVT_GAMES_LIST_UPDATED, 
-
-    EVT_GAME_ENDED,
-
+    RES_GAMES_LIST, // Payload = Game List
+    
+    EVT_GAMES_LIST_UPDATED, // Payload = Game List
+    EVT_GAME_CREATED, // Payload = Game List
+    EVT_GAME_ENDED, // Payload = Game List
+    
     REQ_MOVE,
-    EVT_OPPONENT_MOVE,
+    RES_MOVE_ACCEPTED,
+    RES_BAD_MOVE,
 
+    EVT_NEXT_MOVE, // Payload = Board
     EVT_GAME_WON,
     EVT_GAME_LOST,
     EVT_GAME_DRAW,
-
-    REQ_REMATCH_ACCEPTED,
-    EVT_REMATCH_ACCEPTED,
-
-    REQ_REMATCH_DENIED,
-    EVT_REMATCH_DENIED
+    
+    REQ_REMATCH, // Payload = Boolean
+    RES_REMATCH // Payload = Boolean
 } MessageType;
 
 // Ogni messaggio è della struttura [MessageHeader][Message] così anche in TCP sappiamo quanti byte aspettare
@@ -76,34 +70,5 @@ typedef struct {
     uint32_t type;
     uint32_t length;
 } MessageHeader;
-
-typedef struct {
-    char nome[NAME_LEN];
-} Payload_REQ_CONNECT;
-
-typedef struct {
-    Game *games;
-} Payload_RES_GAMES_LIST;
-
-typedef struct {
-    Game partita;
-} Payload_EVT_GAMES_LIST_UPDATED;
-
-typedef struct {
-    Game partita;
-} Payload_EVT_GAME_CREATED;
-
-typedef struct {
-    Game partita;
-} Payload_EVT_GAME_ENDED;
-
-typedef struct {
-    uint32_t column;
-} Payload_REQ_MOVE;
-
-typedef struct {
-    uint32_t column;
-} Payload_EVT_OPPONENT_MOVE;
-
 
 #endif
