a735551237faf73d9bde7811b7b6c80dab8c6289
Gaetano Di Tota <ditotagaetano@outlook.it>
2026-03-09 06:20:28 +0100

    Aggiunto NCurses - Rimosso Screen con codici ANSI - Aggiunto Window con NCurses

---
+++ Client/core/window/Window.cpp
@@ -0,0 +1,68 @@
+#include "Window.hpp"
+
+lso::Window::Window(const int height, const int width, const int startY, const int startX) {
+    window = newwin(height, width, startY, startX);
+}
+
+// --------------------------------------------------------------------------------
+
+void lso::OutputWindow::print(const std::string & text) {
+    werase(window);
+
+    wmove(window, 0, 0);
+    wprintw(window, text.c_str());
+
+    wrefresh(window);
+}
+
+// --------------------------------------------------------------------------------
+
+lso::InputWindow::InputWindow(const int height, const int width, const int startY, const int startX) : Window(height, width, startY, startX) {
+    noecho();
+    cbreak();
+
+    box(window, 0, 0);
+    wrefresh(window);
+    
+    innerWindow = derwin(window, height - 2, width - 2, 1, 1);
+}
+
+void lso::InputWindow::addTitle(const std::string & text) {
+    werase(window);
+    
+    box(window, 0, 0);
+    wmove(window, 0, 1);
+    wprintw(window, text.c_str());
+
+    wrefresh(window);
+}
+
+void lso::InputWindow::print(const std::string & text) {
+    werase(innerWindow);
+
+    wmove(innerWindow, 0, 0);
+    wprintw(innerWindow, "> ");
+    wprintw(innerWindow, text.c_str());
+
+    wrefresh(innerWindow);
+}
+
+std::string lso::InputWindow::getInput() {
+    std::string input;
+    int ch;
+
+    do {
+
+        ch = wgetch(innerWindow);
+
+        if ( (ch == 127 || ch == 8) && !input.empty())
+            input.pop_back();
+        else if (isprint(ch))
+            input.push_back(ch);
+
+        print(input);
+
+    } while (ch != '\n' && ch != '\r');
+
+    return input;
+}
---
+++ Client/core/window/Window.hpp
@@ -0,0 +1,84 @@
+#ifndef WINDOW_HPP
+#define WINDOW_HPP
+
+#include <iostream>
+#include <thread>
+#include <string>
+
+#include <ncurses.h>
+
+namespace lso {
+
+    class Window {
+        private:
+
+            // ...
+        
+        protected:
+        
+            WINDOW * window;
+        
+        public:
+
+            virtual void print(const std::string & text) = 0;
+        
+        public:
+        
+            Window(const int height, const int width, const int startY, const int startX);
+
+            Window(const Window &) = delete;
+            Window(Window &&) noexcept = delete;
+
+            virtual ~Window() { delwin(window); };
+
+            Window &operator = (const Window &) = delete;
+            Window &operator = (Window && other) noexcept = delete;
+
+            bool operator == (const Window &) const noexcept = delete;
+            bool operator != (const Window &) const noexcept = delete;
+
+    };
+
+    class OutputWindow : public Window {
+        private:
+
+            // ...
+        
+        protected:
+        
+            // ...
+        
+        public:
+
+            using Window::Window;
+
+            void print(const std::string & text) override;
+        
+    };
+
+    class InputWindow : public Window {
+        private:
+
+            // ...
+
+        protected:
+
+            WINDOW * innerWindow;
+
+            void print(const std::string & text) override;
+
+        public:
+
+            void addTitle(const std::string & text);
+
+            std::string getInput();
+        
+        public:
+
+            InputWindow(const int height, const int width, const int startY, const int startX);
+
+    };
+
+}
+
+#endif
