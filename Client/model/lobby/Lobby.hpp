85cb9d92f5ed4d28d88ce637eab4fe4da07d0865
Gaetano Di Tota <ditotagaetano@outlook.it>
2026-03-23 11:16:59 +0100

    Aggiunta della classe Lobby con metodi per gestire l'identificativo, il proprietario e l'avversario del gioco.

---
+++ Client/model/lobby/Lobby.hpp
@@ -0,0 +1,45 @@
+#ifndef LOBBY_HPP
+#define LOBBY_HPP
+
+#include <string>
+
+namespace lso {
+
+    class Lobby {
+        private:
+
+            unsigned int id;
+
+            std::string owner;
+            std::string opponent;
+
+        protected:
+
+            //...
+
+        public:
+
+            unsigned int getID() const noexcept { return id; };
+            const std::string & getOwner() const noexcept { return owner; };
+            const std::string getOpponent() const noexcept { return opponent.empty() ? "In attesa..." : opponent; };
+
+        public:
+
+            Lobby(Game game) : id(game.id), owner(game.proprietario), opponent(game.avversario) {};
+
+            Lobby(const Lobby &) = delete;
+            Lobby(Lobby &&) = default;
+
+            virtual ~Lobby() = default;
+
+            Lobby & operator = (const Lobby &) = default;
+            Lobby & operator = (Lobby &&) = default;
+
+            bool operator == (const Lobby & other) const noexcept { return id == other.id; };
+            bool operator != (const Lobby & other) const noexcept { return ! (*this == other); };
+
+    };
+
+}
+
+#endif
